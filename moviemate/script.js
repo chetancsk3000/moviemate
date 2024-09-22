688bc42b78f72430a8952975a878fdb4b7bcf029
JAYASHISH05 <142114851+JAYASHISH05@users.noreply.github.com>
2024-09-22 10:19:26 +0530

    Delete moviemate directory

--- moviemate/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const body = document.body;
-
-    // Change background color automatically every 5 seconds
-    setInterval(() => {
-        body.classList.toggle('highlight');
-    }, 5000);
-});
